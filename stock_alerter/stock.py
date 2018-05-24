3f2cc9074cb03998ef49118b907e1b90a9d28eba
Henry Min <henrymin@gmail.com>
2018-05-24 08:55:40 -0400

    chapter 3 before refactoring.

--- stock_alerter/stock.py
+++ stock_alerter/stock.py
@@ -1,11 +1,21 @@
 import collections
 import bisect
 from datetime import timedelta
+from enum import Enum
 
 PriceEvent = collections.namedtuple("PriceEvent", ["timestamp", "price"])
 
 
+class StockSignal(Enum):
+    buy = 1
+    neutral = 0
+    sell = -1
+
+
 class Stock(object):
+    LONG_TERM_TIMESPAN = 10
+    SHORT_TERM_TIMESPAN = 5
+
     def __init__(self, symbol):
         self.symbol = symbol
         self.price_history = []
@@ -23,34 +33,64 @@ class Stock(object):
         return self.price_history[-3].price < self.price_history[-2].price < self.price_history[-1].price
 
     def get_corssover_signal(self, on_date):
-        cpl = []
-        for i in range(11):
+        NUM_DAYS = self.LONG_TERM_TIMESPAN + 1
+
+        closing_price_list = \
+            self._get_closing_price_list(on_date, NUM_DAYS)
+
+        if len(closing_price_list) < 11:
+            return StockSignal.neutral
+
+        long_term_series = closing_price_list[-self.LONG_TERM_TIMESPAN:]
+        prev_long_term_series = \
+            closing_price_list[-self.LONG_TERM_TIMESPAN-1:-1]
+        short_term_series = closing_price_list[-self.SHORT_TERM_TIMESPAN:]
+        prev_short_term_series = \
+            closing_price_list[-self.SHORT_TERM_TIMESPAN-1:-1]
+
+        long_term_ma = sum([update.price for update in long_term_series]) \
+            / self.LONG_TERM_TIMESPAN
+        prev_long_term_ma = sum([update.price for update in prev_long_term_series]) \
+            / self.LONG_TERM_TIMESPAN
+        short_term_ma = sum([update.price for update in short_term_series]) \
+            / self.SHORT_TERM_TIMESPAN
+        prev_short_term_ma = sum([update.price for update in prev_short_term_series]) \
+            / self.SHORT_TERM_TIMESPAN
+
+        # BUY signal
+        if self._is_short_term_below_to_above(prev_short_term_ma,
+                                              prev_long_term_ma,
+                                              short_term_ma,
+                                              long_term_ma):
+            return StockSignal.buy
+
+        # SELL signal
+        if self._is_short_term_below_to_above(prev_short_term_ma,
+                                              prev_long_term_ma,
+                                              short_term_ma,
+                                              long_term_ma):
+            return StockSignal.sell
+
+        return StockSignal.neutral
+
+    def _get_closing_price_list(self, on_date, num_days):
+        closing_price_list = []
+        for i in range(num_days):
             chk = on_date.date() - timedelta(i)
             for price_event in reversed(self.price_history):
                 if price_event.timestamp.date() > chk:
                     pass
                 if price_event.timestamp.date() == chk:
-                    cpl.insert(0, price_event)
-                if price_event.timestamp.date() < chk:
-                    cpl.insert(0, price_event)
+                    closing_price_list.insert(0, price_event)
                     break
-        # Return NEUTRAL signal
-        if len(cpl) < 11:
-            return 0;
+                if price_event.timestamp.date() < chk:
+                    closing_price_list.insert(0, price_event)
+                    break
 
-        # BUY signal
-        if sum([update.price for update in cpl[-11:-1]])/10 \
-            > sum([update.price for update in cpl[-6:-1]])/5 \
-            and sum([update.price for update in cpl[-10:]])/10 \
-            < sum([update.price for update in cpl[-5:]])/5:
-            return 1
+        return closing_price_list
 
-        # SELL signal
-        if sum([update.price for update in cpl[-11:-1]])/10 \
-            < sum([update.price for update in cpl[-6:-1]])/5 \
-            and sum([update.price for update in cpl[-10:]])/10 \
-            > sum(update.price for update in cpl[-5:])/5:
-            return -1
-
-        #NEUTRAL signal
-        return 0
+    @staticmethod
+    def _is_short_term_below_to_above(prev_ma, prev_reference_ma,
+                                      current_ma, current_reference_ma):
+        return prev_ma > prev_reference_ma \
+            and current_ma < current_reference_ma
